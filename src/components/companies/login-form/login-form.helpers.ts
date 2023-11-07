023b1be1d0c8dd56b9bfdce4fac588dc9be35238
eduardoquispe <eduardoquispe344@gmail.com>
2023-11-07 14:53:13 -0500

    feat(login-form): update error messages

--- src/components/companies/login-form/login-form.helpers.ts
+++ src/components/companies/login-form/login-form.helpers.ts
@@ -4,7 +4,10 @@ export const getLoginFormResolverHelper = () =>
   yup.object().shape({
     email: yup
       .string()
-      .email("El correo es incorrecto")
-      .required("Correo es requerido"),
-    password: yup.string().required("Contraseña es requerida"),
+      .email("El correo electrónico es incorrecto")
+      .required("El correo electrónico es requerido"),
+    password: yup
+      .string()
+      .min(6, "La contraseña no es válida")
+      .required("La contraseña es requerida"),
   });
